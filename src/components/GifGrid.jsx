c6e50f637d2534f8a7bd6940d8aca99be8d29735
Thezx <thezxdev@gmail.com>
2022-08-08 23:22:32 -0700

    Mensaje de carga al buscar imagenes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -28,6 +28,16 @@ export const GifGrid = ({ category }) => {
   return (
     <>
       <h3>{ category }</h3>
+      
+      {
+        // isLoading 
+        // ? ( <h2>Cargando...</h2> )
+        // : null
+
+        // If corto
+        isLoading && ( <h2>Cargando...</h2> )
+
+      }
 
       <div className="card-grid">
         {
