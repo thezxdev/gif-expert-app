8756a9658baf03cae6a8c3c664bd60475b764651
Thezx <thezxdev@gmail.com>
2022-08-08 22:31:16 -0700

    clases html en react mas esparcir propieades a componentes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
+import { GifItem } from './GifItem';
 
 export const GifGrid = ({ category }) => {
 
@@ -17,7 +18,6 @@ export const GifGrid = ({ category }) => {
     // Código a ejecutar
     // getGifs( category );
 
-
     getImages();
 
   }, [] );
@@ -26,17 +26,16 @@ export const GifGrid = ({ category }) => {
     <>
       <h3>{ category }</h3>
 
-      <ol>
+      <div className="card-grid">
         {
-          images.map( ({ id, title }) => (
-            <li
-              key={ id }
-            >
-              { title }
-            </li>
+          images.map( ( image ) => (
+            <GifItem
+              key={ image.id }
+              { ...image }
+            />
           ))
         }
-      </ol>
+      </div>
 
     </>
   )
