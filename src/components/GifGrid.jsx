3d0862a046b1ce21eeb4a71400dfd8fe6eaff1ec
Thezx <thezxdev@gmail.com>
2022-08-08 18:30:48 -0700

    Obtener imagenes y asignarlas al estado

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,19 +1,43 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
+
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+
+    const newImages = await getGifs( category );
+    setImages( newImages );
+  }
 
   // Sirve para disparar efectos secundarios
   useEffect( () => {
 
     // Código a ejecutar
-    getGifs( category );
+    // getGifs( category );
+
+
+    getImages();
 
   }, [] );
 
   return (
     <>
       <h3>{ category }</h3>
+
+      <ol>
+        {
+          images.map( ({ id, title }) => (
+            <li
+              key={ id }
+            >
+              { title }
+            </li>
+          ))
+        }
+      </ol>
+
     </>
   )
 }
